9a9add2301fe7b1b1258fc260c8dc94fa903202b
litals1379 <litals1379@walla.com>
2025-03-07 16:04:56 +0200

    Register fixed

--- DAL/UserDBservices.cs
+++ DAL/UserDBservices.cs
@@ -15,11 +15,6 @@ namespace Server_Side.DAL
             _usersCollection = database.GetCollection<User>("users");
         }
 
-        public async Task<List<User>> GetUsersAsync()
-        {
-            return await _usersCollection.Find(_ => true).ToListAsync();
-        }
-
         public async Task<bool> AddUserAsync(User user)
         {
             var existingUser = await _usersCollection.Find(u => u.Email == user.Email).FirstOrDefaultAsync();
@@ -30,5 +25,12 @@ namespace Server_Side.DAL
             await _usersCollection.InsertOneAsync(user);
             return true;
         }
+
+        public async Task<List<User>> GetUsersAsync()
+        {
+            return await _usersCollection.Find(_ => true).ToListAsync();
+        }
+
+        
     }
 }
