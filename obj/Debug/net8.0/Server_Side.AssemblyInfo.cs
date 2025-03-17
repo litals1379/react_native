7bcf649ff330bf95f123ec13e06e0638bbb93aba
litals1379 <litals1379@walla.com>
2025-03-17 14:09:52 +0200

    .

--- obj/Debug/net8.0/Server_Side.AssemblyInfo.cs
+++ obj/Debug/net8.0/Server_Side.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server_Side")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87391fb999bce60cd5580ef79129e2c35910a3db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+370ab9006bc5ce99ce4434e6f3cec3b3ca58a3a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server_Side")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server_Side")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
