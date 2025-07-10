7988d2aabb365032ea9c3366df047c9e45d9e5f2
litals1379 <litals1379@walla.com>
2025-07-10 21:35:19 +0300

    ProblematicWords List

--- server_side/BL/Feedback.cs
+++ server_side/BL/Feedback.cs
@@ -8,6 +8,8 @@
     public DateTime TimeCreated { get; set; }
     public int ErrorsInReading { get; set; }
 
+    // מילים בהן הילד התקשה
+    public List<string> ProblematicWords { get; set; } = new List<string>();
 
     public void AddFeedback() { /* קוד להוספת משוב */ }
     public void GetFeedback() { /* קוד לקבלת משוב */ }
