201fb60d22aa8fc910f49ceb82eecbf3e062f408
ChenAhrak <chenahrak@gmail.com>
2025-07-18 15:31:11 +0300

    report screen

---
+++ client_side/app/reportScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function reportScreen() {
+  return (
+    <View>
+      <Text>reportScreen</Text>
+    </View>
+  )
+}
