d80fa739bd0f1fa3092c20eac05481b3b493b6cc
ChenAhrak <chenahrak@gmail.com>
2025-07-26 12:29:18 +0300

    background of the anylyzing modal

--- client_side/app/Style/storyFromLibrary.js
+++ client_side/app/Style/storyFromLibrary.js
@@ -159,7 +159,7 @@ export const styles = StyleSheet.create({
   loadingOverlay: {
     position: 'absolute',
     top: 0, left: 0, right: 0, bottom: 0,
-    backgroundColor: 'rgba(255,255,255,0.8)',
+    backgroundColor: 'rgba(212, 210, 210, 0.8)',
     justifyContent: 'center',
     alignItems: 'center',
     zIndex: 1000,
